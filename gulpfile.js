73e3d0a05f197058c8ffd0250dd0497d5968133c
Seiji Sawayanagi <sawayanagi@1101.com>
2017-03-15 19:49:00 +0900

    Improved directory

--- gulpfile.js
+++ gulpfile.js
@@ -7,44 +7,44 @@ var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync').create();
 
 gulp.task('pug', function() {
-  gulp.src('pug/**/*.pug')
+  gulp.src('src/pug/**/*.pug')
   .pipe(plumber())
   .pipe(pug({pretty: true}))
-  .pipe(gulp.dest('public'))
+  .pipe(gulp.dest('build'))
   .pipe(browserSync.stream());
 });
 
 gulp.task('sass', function() {
-  gulp.src('sass/**/*.scss')
+  gulp.src('src/sass/**/*.scss')
   .pipe(plumber())
   .pipe(sass({outputStyle: "compressed"}))
-  .pipe(gulp.dest('public/css'))
+  .pipe(gulp.dest('build/css'))
   .pipe(browserSync.stream());
 });
 
 gulp.task('babel', function() {
-  gulp.src('es6/**/*.es6')
+  gulp.src('src/es6/**/*.es6')
   .pipe(plumber())
   .pipe(babel({presets: ['es2015']}))
   .pipe(uglify())
-  .pipe(gulp.dest('public/js'))
+  .pipe(gulp.dest('build/js'))
   .pipe(browserSync.stream());
 });
 
 gulp.task('server', function() {
   browserSync.init({
     server: {
-      baseDir: 'public',
+      baseDir: 'build',
       index: 'index.html',
-      directory: true
+      directory: false
     }
   });
 });
 
 gulp.task('watch', function () {
-  gulp.watch('pug/**/*.pug', ['pug']);
-  gulp.watch('sass/**/*.scss', ['sass']);
-  gulp.watch('es6/**/*.es6', ['babel']);
+  gulp.watch('src/pug/**/*.pug', ['pug']);
+  gulp.watch('src/sass/**/*.scss', ['sass']);
+  gulp.watch('src/es6/**/*.es6', ['babel']);
 });
 
 gulp.task('default', ['watch', 'server']);
